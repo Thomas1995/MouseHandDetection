239a80107086b20cd43199b7ac76282692955290
draganrelu <reludragan95@yahoo.com>
2016-11-12 12:01:46 +0200

    ada

--- src/DataProcessor.cpp
+++ src/DataProcessor.cpp
@@ -57,11 +57,31 @@ void DataProcessor::SendCursorData(int x, int y, int state, int socket)
 }
 void DataProcessor::SendInputDataWin(int x, int y, int key, int leftClick, int rightClick, int serverSocket)
 {
+    m_queuedData.push_back(CursorData(x,y,leftClick, rightClick,key));
+    m_tmpx += x;
+    m_tmpy += y;
+
+    while (m_queuedData.size() > MAX_QUEUE_SIZE) {
+	m_tmpx -= m_queuedData.front().x;
+	m_tmpy -= m_queuedData.front().y;
+	m_queuedData.pop_front();
+
+    }
+
+    if (getTime() - m_lastTimeSent > DELTA_TIME_SEND) {
+	m_lastTimeSent = getTime();
+	char* msg = DataProcessor::instance()->Interpolate().ToMsgWin();
+	send(serverSocket, msg, 5, 0);
+	delete msg;
+    }
+
 
 }
 CursorData DataProcessor::Interpolate()
 {
     return CursorData(round(m_tmpx / m_queuedData.size()), 
 	    round(m_tmpy / m_queuedData.size()), 
-	    m_queuedData.back().leftClick);
+	    m_queuedData.back().leftClick, 
+	    m_queuedData.back().rightClick,
+	    m_queuedData.back().key);
 }
